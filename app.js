c6b9a32ddc2d87b57b823dc3dc197a1f153a9745
Kedarnath Peraka <71390186+kedarnathdev@users.noreply.github.com>
2022-10-25 10:38:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fetch = (...args) => import ('node-fetch').then(({default: fetch}) => fetc
 const {json} = require('body-parser');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 80;
 const router = require('./routes/index');
 app.use('/', router);
 
